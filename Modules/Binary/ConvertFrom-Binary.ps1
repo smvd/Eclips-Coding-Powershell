29f2053490e1083c32dad31b82e800c006b0b9ec
smvd <semvd06@gmail.com>
2021-03-13 12:07:06 +0100

    Finished ConvertFrom-Binary and Added ConvertTo-Binray

--- Modules/Binary/ConvertFrom-Binary.ps1
+++ Modules/Binary/ConvertFrom-Binary.ps1
@@ -1,4 +1,4 @@
-function ConvertTo-Binary {
+function ConvertFrom-Binary {
     [CmdletBinding()]
     param (
         [parameter(Mandatory, ValueFromPipeline)]
@@ -6,14 +6,27 @@ function ConvertTo-Binary {
     )
     
     begin {
-        
+        if ($binary -notmatch '^[01]*$')
+        {
+            Throw 'Input isnt binary'
+        }
     }
     
     process {
-        
+        $i = 0;
+        $num = 0;
+        foreach ($char in $binary.ToCharArray())
+        {
+            if ($char -eq '1')
+            {
+                $num += [Math]::Pow(2,$i)
+            }
+
+            $i++
+        }
     }
     
     end {
-        
+        return $num
     }
 }
