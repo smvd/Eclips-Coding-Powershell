b874fca06c2f1f68650ae35e0acc0fc4f890383c
smvd <semvd06@gmail.com>
2021-03-13 12:39:20 +0100

    Finished ConvertTo-Binaru

--- Modules/Binary/ConvertFrom-Binary.ps1
+++ Modules/Binary/ConvertFrom-Binary.ps1
@@ -9,8 +9,7 @@ function ConvertFrom-Binary {                       # Function decleration
     begin {
         Write-Verbose 'BEGIN'
         Write-Verbose 'Validating input...'
-        if ($binary -notmatch '^[01]*$')            # REGEX: If each character in the string is 0,1 return true
-        {
+        if ($binary -notmatch '^[01]*$') {          # REGEX: If each character in the string is 0,1 return true
             Write-Verbose 'Input isnt binary'
             Throw 'Input isnt binary'               # Error: 'Input isnt binary'
         }
@@ -22,10 +21,8 @@ function ConvertFrom-Binary {                       # Function decleration
         $i = 0;                                     # The variable for the bit position
         $num = 0;                                   # The variable for the total
         Write-Verbose 'Looping each bit'
-        foreach ($char in $binary.ToCharArray())    # Loop threw each bit in the binary
-        {
-            if ($char -eq '1')                      # If its a 1
-            {
+        foreach ($char in $binary.ToCharArray()) {  # Loop threw each bit in the binary
+            if ($char -eq '1') {                    # If its a 1
                 Write-Verbose 'Running the math...'
                 $num += [Math]::Pow(2,$i)           # Get 2 to the power of the number
             }
--- Modules/Binary/ConvertTo-Binary.ps1
+++ Modules/Binary/ConvertTo-Binary.ps1
@@ -1,20 +1,40 @@
-﻿function ConvertTo-Binary {
-    [CmdletBinding()]
+﻿function ConvertTo-Binary {                                                                                 # Function decleration
+    [CmdletBinding()]                                                                                       # So we can use write-verbose(wich i wont be commenting)
     param (
-        [parameter(Mandatory, ValueFromPipeline)]
-        [ValidateRange(1, [int]::MaxValue)]
-        [int]$deciaml
+        [parameter(Mandatory, ValueFromPipeline)]                                                           # Needed and can be piped
+        [ValidateRange(1, 1000000)]                                                                         # Input must be between 1 and a million
+        [int]$decimal                                                                                       # Its stored in a int called deciaml
     )
     
     begin {
-        
+        Write-Verbose 'BEGIN'
+        Write-Verbose 'Making variables...'
+
+        [array]$binary                                                                                      # An array to store the binary
+        [int]$i = 0                                                                                         # Loop count
     }
     
     process {
-        
+        Write-Verbose 'PROCESS'
+        Write-Verbose 'Looping...'
+        while($i -le 20) {                                                                                  # Loop 20 times
+            $binary += ($decimal % 2) -as [string]                                                          # Add the remainder to the array 
+            [int]$decimal = ($decimal / 2) -as [int]                                                        # Set the value \2 
+
+            $i++                                                                                            # Add 1 to i
+        }
     }
     
     end {
+        Write-Verbose 'END'
         
+        Write-Verbose 'Inverting output...'
+        [string]$left = ([regex]::Matches($binary,'.','RightToLeft') | ForEach-Object {$_.value}) -join ''  # Inverting the array of binary
+
+        Write-Verbose 'Removing leading 0s...'
+        $out = $left -replace '^0+', '0'                                                                    # Remove leading 0s accept the last one
+
+        Write-Verbose 'Returing...'
+        return $out                                                                                         # Returning
     }
 }
