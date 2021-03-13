04491dffb87247359fb3de5f563f00df6a6990c7
smvd <semvd06@gmail.com>
2021-03-13 12:18:12 +0100

    Comented ConvertFrom-Binary

--- Modules/Binary/ConvertFrom-Binary.ps1
+++ Modules/Binary/ConvertFrom-Binary.ps1
@@ -1,32 +1,42 @@
-function ConvertFrom-Binary {
-    [CmdletBinding()]
+function ConvertFrom-Binary {                       # Function decleration
+    [CmdletBinding()]                               # Lets us use write-verbose
+                                                    # I wont comment each write-verbose
     param (
-        [parameter(Mandatory, ValueFromPipeline)]
-        [string]$binary
+        [parameter(Mandatory, ValueFromPipeline)]   # Needed and you can pipe it in
+        [string]$binary                             # Its a string alright
     )
     
     begin {
-        if ($binary -notmatch '^[01]*$')
+        Write-Verbose 'BEGIN'
+        Write-Verbose 'Validating input...'
+        if ($binary -notmatch '^[01]*$')            # REGEX: If each character in the string is 0,1 return true
         {
-            Throw 'Input isnt binary'
+            Write-Verbose 'Input isnt binary'
+            Throw 'Input isnt binary'               # Error: 'Input isnt binary'
         }
     }
     
     process {
-        $i = 0;
-        $num = 0;
-        foreach ($char in $binary.ToCharArray())
+        Write-Verbose 'PROCESS'
+        Write-Verbose 'Making variables...'
+        $i = 0;                                     # The variable for the bit position
+        $num = 0;                                   # The variable for the total
+        Write-Verbose 'Looping each bit'
+        foreach ($char in $binary.ToCharArray())    # Loop threw each bit in the binary
         {
-            if ($char -eq '1')
+            if ($char -eq '1')                      # If its a 1
             {
-                $num += [Math]::Pow(2,$i)
+                Write-Verbose 'Running the math...'
+                $num += [Math]::Pow(2,$i)           # Get 2 to the power of the number
             }
 
-            $i++
+            $i++                                    # Add 1 to i
         }
     }
     
     end {
-        return $num
+        Write-Verbose 'END'
+        Write-Verbose 'Returning'
+        return $num                                 # Return the number
     }
 }
--- Modules/Binary/ConvertTo-Binary.ps1
+++ Modules/Binary/ConvertTo-Binary.ps1
@@ -1,1 +1,20 @@
-﻿
+﻿function ConvertTo-Binary {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [ValidateRange(1, [int]::MaxValue)]
+        [int]$deciaml
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
